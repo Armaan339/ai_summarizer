9b036cde95a646d8bcb3f3e73c950169ac81282e
Armaan339 <armaanbrar339@gmail.com>
2023-09-25 16:03:18 -0700

    input tag is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         </nav>
             <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden'/>
             <span className='orange_gradient'>OpenAI GPT-4</span></h1>
-            <h2 className='desc'>	Simplify your reading with Summize, and open-source article summarizer
+            <h2 className='desc m-auto'>	Simplify your reading with Summize, and open-source article summarizer
 				that transforms lengthy articles into clear and concise summaries</h2>
      </header>
   )
